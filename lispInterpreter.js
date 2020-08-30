44794c803fdd5574f81cbfab62eea9734c73cec6
Sonika Gupta <sonikagupta94@gmail.com>
2020-08-31 02:34:58 +0530

    Handled if statements + multiline programs

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -3,10 +3,10 @@ const specialForms = {};
 specialForms.if = (scope, args) => {
 	if (args.length != 3) {
 		throw new SyntaxError("Invalid args to if");
-	} else if (evaluate(scope, args[0])) {
-		return evaluate(scope, args[1]);
+	} else if (interpret(scope, args[0])[0]) {
+		return interpret(scope, args[1])[0];
 	} else {
-		return evaluate(scope, args[2]);
+		return interpret(scope, args[2])[0];
 	}
 };
 
@@ -28,7 +28,7 @@ specialForms.do = (scope, args) => {
 	return value;
 };
 
-specialForms.define = (scope, key, value) => {
+specialForms.define = (scope, [key, value]) => {
 	if (value == undefined || Number(key)) {
 		throw new SyntaxError("Incorrect use of define");
 	}
@@ -36,7 +36,7 @@ specialForms.define = (scope, key, value) => {
 	// return value;
 };
 
-specialForms.lambda = (scope, params, body) => {
+specialForms.lambda = (scope, [params, body]) => {
 	if (!params || !body) {
 		throw new SyntaxError("Function Body Required");
 	}
@@ -55,15 +55,45 @@ specialForms.lambda = (scope, params, body) => {
 		for (let i = 0; i < arguments.length; i++) {
 			localScope[params[i]] = arguments[i];
 		}
-		return run(localScope, body);
+		return interpret(localScope, body)[0];
 	};
+}
+
+function evaluate(scope, action, args) {
+	// console.log('action:', action, 'args:', args);
+	if(Number(action)) return Number(action);
+
+	if(typeof action == 'function') return action(...args);
+
+	if(action in specialForms) return specialForms[action](scope, args);
+
+	if(action in scope) {
+		const value = scope[action];
+		return typeof value === 'function' ? value(...args) : value;
+	}
+		
+	if(action in global.operations)
+		return global.operations[action](...args.map(el => evaluate(scope, el)));
+
+	throw new SyntaxError(`Unknown Keyword ${action}`);	
 }
 
 const spaceParse = input => input && input.replace(/^\s*|\s*$/, '');
 
 function getWord(input) {
-	const word = input.match(/^[^\s)]+/);
+	const word = input.match(/^[^\s)()]+/);
 	return word ? [word[0], spaceParse(input.slice(word[0].length))] : [null, spaceParse(input)];
+}
+function getExpr(input) {
+	input = spaceParse(input);
+	let brackets = 1, i = 1;
+	while(brackets && i < input.length) {
+		if(input[i] == '(') brackets++;
+		else if(input[i] == ')') brackets--;
+		i++;
+	}
+	if(brackets) return null;
+	return [input.slice(0, i), input.slice(i)]
 }
 
 function extractDefine(scope, input) {
@@ -75,36 +105,40 @@ function extractDefine(scope, input) {
 
 function extractLambda(input) {
 	let parameters = [], body;
+	input = spaceParse(input.slice(1));
 	while(input[0]!=')') {
 		let value;
-		[value, input] = getWord(input.slice(1));
+		[value, input] = getWord(input);
 		parameters.push(value);
 	}
-	input = spaceParse(input.slice(1));
-	let brackets = 1, i = 1;
-	while(brackets && i < input.length) {
-		if(input[i] == '(') brackets++;
-		else if(input[i] == ')') brackets--;
-		i++;
-	}
-	if(brackets) return null;
-	[body, input] = [input.slice(0, i), input.slice(i)];
-
+	
+	[body, input] = getExpr(input.slice(1));
 	return [[parameters, body], input];
+}
+
+function extractConditional(input) {
+	let args = [];
+	for(let i=0; i< 3; i++) {
+		[args[i], input] = getExpr(input);
+	}
+	return [args, input];
 }
 
 function extractSpecialForm(scope, action, input) {
 	switch(action) {
 		case 'define': return extractDefine(scope, input);
 		case 'lambda': return extractLambda(input);
+		default: return extractConditional(input);
 	}
 }
 
 function interpret(scope, input) {
 	if(input[0]!='(') return null;
 	input = spaceParse(input.slice(1));
+
 	let action, args = [];
-	[action, input] = getWord(input);
+	[action, input] = interpret(scope, input) || getWord(input);
+
 	if(action in specialForms) {
 		[args, input] = extractSpecialForm(scope, action, input);
 	}
@@ -114,46 +148,44 @@ function interpret(scope, input) {
 			[value, input] = interpret(scope, input) || getWord(input);
 			// console.log('value:', value);
 			if(!value) throw Error('What?');
-			args.push(value);
+			args.push(evaluate(scope, value));
 		}
 	}
 	if(input) input = input.slice(1);
 
-	// console.log('sending to eval:', input);
-	return [evaluate(scope, action, args), input];
-}
-
-function evaluate(scope, action, args) {
-	// console.log('action:', action, 'args:', args);
-	if(action in specialForms) return specialForms[action](scope, ...args);
-
-	if(action in scope) return typeof scope[action] == 'function' ? scope[action](...args) : scope[action];
-	
-	if(Number(action)) return Number(action);
-		
-	if(action in global.operations)
-		return global.operations[action](...args.map(el => evaluate(scope, el)));
-
-	
-	throw new SyntaxError(`Unknown Keyword ${action}`);	
+	return [evaluate(scope, action, args), spaceParse(input)];
 }
 
 function run(scope, data) {
-	let [value, garbage] = interpret(scope, data);
-	if(garbage) throw SyntaxError(`Unexpected token ${garbage}`);
-	if(value) console.log(value);
+	data = data.split('\n').filter(line => line[0]!='#').join(' ');
+	while(data) {
+		let value;
+		[value, data] = interpret(scope, spaceParse(data));
+		if(value) console.log(value);
+	}
 }
 
-const fs = require('fs'),
-	readline = require('readline');
+const fs = require('fs');
+fs.readdir('./lispInterpreter/lisptest', (err, files) => {
+	if (err)
+			console.log(err);
+	else {
+		files.forEach(file => {
+			console.log(file);
+			const data = fs.readFileSync(`./lispInterpreter/lisptest/${file}`);
+			run(global, data.toString());
+		})
+	}
+});
+
+/* const readline = require('readline');
 
 const rd = readline.createInterface({
-	input: fs.createReadStream('./test.lisp'),
+	input: fs.createReadStream('./lispInterpreter/test3.lisp'),
 	output: process.stdout,
 	console: false
 });
 
 rd.on('line', function(line) {
 	console.log(line);
-	run(global, line);
-});
+}); */
